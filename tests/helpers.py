7f847ae82a3fdc7d4f9fbb83e9e98e3848cabda0
K hashimoto <keishi04hrikzira@gmail.com>
2025-10-06 18:03:11 +0900

    💚 テスト終了後に送信済みメッセージの確認を行うヘルパー関数を作成

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,4 +1,7 @@
 from mypy_boto3_dynamodb.type_defs import AttributeValueTypeDef
+from moto.ses.models import SESBackend, Message
+from send_email import SUBJECT, BODY_TEMPLATE
+from contextlib import contextmanager
 
 
 def get_value_from_attribute_type_def(val: AttributeValueTypeDef):
@@ -9,3 +12,38 @@ def get_value_from_attribute_type_def(val: AttributeValueTypeDef):
         return float(value)
     else:
         return value
+
+
+def assert_sent_message(
+    message: Message, from_email: str, to_email: str, username: str, presigned_url: str
+):
+    # message の型定義は以下
+    # https://github.com/getmoto/moto/blob/f4db5af875245e0aff0a3166bdf267d6609fe08c/moto/ses/models.py#L82
+    assert message.source == from_email
+    assert message.destinations == {"ToAddresses": [to_email]}
+    assert message.subject == SUBJECT
+    assert message.body == BODY_TEMPLATE.format(
+        username=username, presigned_url=presigned_url
+    )
+
+
+@contextmanager
+def assert_ses_backend(
+    ses_backend: SESBackend,
+    from_email: str,
+    to_email: str,
+    username: str,
+    presigned_url: str,
+):
+    assert ses_backend.sent_message_count == 0
+    try:
+        yield
+    finally:
+        assert ses_backend.sent_message_count == 1
+        assert_sent_message(
+            message=ses_backend.sent_messages[0],
+            from_email=from_email,
+            to_email=to_email,
+            username=username,
+            presigned_url=presigned_url,
+        )
